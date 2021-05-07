f3f45e93c6ce29aea0f4209c04c0486cc3657ce8
Gabriel de Almeida <gabrielc_cordeiro@hotmail.com>
2021-05-07 15:28:25 -0300

    Basic statuc components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/galmeidac" target="_blank" rel="noreferrer">Gabriel de Almeida</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/g.almeida.c" target="_blank" rel="noreferrer">@g.almeida.c</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
